a8240aeb047c3c65919645176d64e32e9d56d240
unknown <irfanusuf33@gmail.com>
2024-03-01 15:54:41 +0530

    class2

---
+++ class 2 implementing react library/my-app/src/App.js
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+
+const App =() => {
+  return (
+
+<div> 
+
+  <Navbar/>
+  <h1> My first react app with suhaib</h1>
+  <Footer/>
+
+
+</div>
+
+
+  )
+
+
+
+}
+
+
+export default App
---
+++ class 2 implementing react library/my-app/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+
+<ul>
+
+    <li>home</li>
+    <li>home</li>
+    <li>home</li>
+    <li>home</li>
+    <li>home</li>
+</ul>
+
+    </div>
+  )
+}
+
+export default Navbar
