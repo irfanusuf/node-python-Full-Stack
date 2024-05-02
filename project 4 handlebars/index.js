9e393fec35ca4ffbc8628ceac2c680f4a6ed2dea
unknown <irfanusuf33@gmail.com>
2024-05-02 15:16:01 +0530

    addedLayout

--- project 4 handlebars/index.js
+++ project 4 handlebars/index.js
@@ -1,45 +1,56 @@
 const express = require("express"); //import
 const path = require("path");
-const cookie = require("cookie-parser")
-const  connectDB = require('./config/dbConnect');
-const { registerhandler, loginhandler, deleteHandler } = require("./controllers/userController");
+const cookie = require("cookie-parser");
+const xhbs = require("express-handlebars")
+const connectDB = require("./config/dbConnect");
+const {
+  registerhandler,
+  loginhandler,
+  deleteHandler,
+} = require("./controllers/userController");
 const bodyParser = require("body-parser");
 const isAuthenticated = require("./authorization/auth");
 
-
 const port = 3000;
-
 const app = express();
 
-connectDB()
 
+
+app.engine("hbs" , xhbs.engine({
+
+  extname: "hbs",     // engine
+  defaultLayout: "layout",   // layout is the main page 
+  layoutsDir: path.join(__dirname, "views", "layouts"),
+  partialsDir: path.join(__dirname, "views" , "partials"),
+
+
+
+}))
+
+app.set("view engine", "hbs");
+
+connectDB();
 
 //middle wares
-app.use(bodyParser.urlencoded({extended : false}))  // relevant for post methods
-app.use(express.json())       //parsing  json data 
-app.use(express.static(path.join(__dirname, "public")));   // serving static files 
-app.use(cookie())
-app.set('view engine', 'hbs');
-
-
+app.use(bodyParser.urlencoded({ extended: false })); // relevant for post methods
+app.use(express.json()); //parsing  json data
+app.use(express.static(path.join(__dirname, "public"))); // serving static files
+app.use(cookie());
 
 // rendering is on server side      SSR
 
-app.get("/", (req, res) => {res.render('index')});
-app.get("/register", (req, res) => {res.render('register')});
-app.get("/login", (req, res) => {res.render('login')});
-app.get("/secureindex", isAuthenticated,  (req, res) => {res.render('secureHome')});
+app.get("/", (req, res) => { res.render("index");});
+app.get("/register", (req, res) => {res.render("register");});
+app.get("/login", (req, res) => {res.render("login");});
+app.get("/secureindex", isAuthenticated, (req, res) => {res.render("secureHome");});
 
 
 
 
-app.post("/register", registerhandler)
-app.post("/login", loginhandler)
-app.delete('/user/del' , deleteHandler)
 
-
-
-
+app.post("/register", registerhandler);
+app.post("/login", loginhandler);
+app.delete("/user/del", deleteHandler);
 
 app.listen(port, () => {
   console.log(`server started on  ${port}`);
